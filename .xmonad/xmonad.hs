f915647d0b0ca5c17d4a1cb8b062dd62bd710190
gnomeye <gnomeye@gmail.com>
2013-06-19 22:23:28 +0200

    new time/date outpu

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -47,7 +47,8 @@ myDmenu="~/bin/dm"
 myRecomp="xmonad --recompile; xmonad --restart; notify-send 'xmonad recompiled'"
 myRest="/usr/bin/xmonad --restart; notify-send 'xmonad restarted'"
 myStream="mplayer -nocache http://roach:8000" 
-myDate="cpid=`pidof conky`;kill -STOP $cpid; date +'<fc=#707070>%A %B %d, %H:%M</fc>' > /tmp/.inxmobar; sleep 3; kill -CONT $cpid"
+--myDate="cpid=`pidof conky`;kill -STOP $cpid; date +'<fc=#707070>%A %B %d, %H:%M</fc>' > /tmp/.inxmobar; sleep 3; kill -CONT $cpid"
+myDate="date '+%a, %b %d | %H:%M' | dzen2 -p 2 -fn xft=Zekton:size=10 -fg '#4E7394' -ta c -w 160 -h 40 -x 880 -y 520"
 
 -- get focus on mouse 
 myFocusFollowsMouse :: Bool
