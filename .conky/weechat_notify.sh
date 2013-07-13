17980b808fa3e37c5fed4f73b6aa99a1a4298ba4
gnomeye <gnomeye@gmail.com>
2013-07-13 11:00:39 +0200

    new pydoc/perldoc/hoogle search pentadactylrc - few mods in other

---
+++ .conky/weechat_notify.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+i=300
+file="/tmp/.weenotify"
+s=`stat -c "%Y" $file`
+d=$(date +%s)
+c=$(echo $d - $s | bc -l)
+wc=`wc -l $file | awk '{print $1}'`
+[[ -x $file ]] || touch $file
+[[ $wc -gt 0 ]] && tail -1 $file
+[[ $c -ge $i ]]  &&  >$file 
