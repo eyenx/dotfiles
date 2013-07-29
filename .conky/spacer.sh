3e6481a5f2c399e964a61505bd015f34054151e3
gnomeye <gnomeye@gmail.com>
2013-07-29 16:37:15 +0200

    changed dzen font to Liberation Mono / spacer adapted

--- .conky/spacer.sh
+++ .conky/spacer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-printf "%$(echo $(xwininfo -name panel|grep Width|tr -s ' '|cut -d' ' -f3)*0.5|bc)s"
+printf "%$(echo $(xwininfo -name panel|grep Width|tr -s ' '|cut -d' ' -f3)*0.2|bc)s"
