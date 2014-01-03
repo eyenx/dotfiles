5323be358b4463b52dc64f785f32c1b88e0275cd
eye <antoniotauro@gmail.com>
2014-01-03 14:11:12 +0100

    back to FF

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -36,8 +36,8 @@ import qualified Data.Map    as M
 
 myTerm    = "urxvtc"
 myTmux = "~/bin/term"
-myBrowser = "chromium"
-myAltBrowser = "firefox"
+myAltBrowser = "chromium"
+myBrowser = "firefox"
 myMail = "mimeo http://mail.google.com"
 myWpChgr = "~/bin/wpchgr.pl"
 myRandWp = "wpfl=$(find ~/img/wallpapers/ -type f|sort -R|head -1);feh --bg-scale --no-fehbg $wpfl;echo $wpfl > /tmp/.randwp"
