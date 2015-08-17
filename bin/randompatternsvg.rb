4417efab9811d483d28849497480484d7f2e3004
eye <eye@eyenx.ch>
2015-08-17 19:26:23 +0200

    fixed randompatterngen

--- bin/randompatternsvg.rb
+++ bin/randompatternsvg.rb
@@ -2,30 +2,6 @@
 
 require 'geo_pattern';
 
-# generators
-#
-gens=[
-'octogons',
-'overlapping_circles',
-'plus_signs',
-'xes',
-'sine_waves',
-'hexagons',
-'overlapping_rings',
-'plaid',
-'triangles',
-'squares',
-'concentric_circles',
-'diamonds',
-'tessellation',
-'nested_squares',
-'mosaic_squares',
-'chevrons'];
-
-# get chosen generator randomly 
-
-gen=gens[Random.new.rand(gens.length)];
-
 # generate random string
 
 rcount=Random.new.rand(100);
@@ -38,4 +14,4 @@ end
 
 # generate pattern from random string
 
-puts GeoPattern.generate(rstring,{:generator => gen}).svg_string;
+puts GeoPattern.generate(rstring).to_svg;
