2e98bb538005717ec74de08ac8f3807d6b9a8eda
eye <eye@eyenx.ch>
2018-07-28 16:17:25 +0200

    modifications for multimonitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,5 +4,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar mybar &
+# launch my bar on every active monitor
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+      MONITOR=$m polybar --reload mybar &
+done
+
