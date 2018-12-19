43c38835c0edd2c95c71771dfddbeb47ae258ef6
eye <eye@eyenx.ch>
2018-12-19 14:47:17 +0100

    some changes

--- bin/lock.sh
+++ bin/lock.sh
@@ -4,9 +4,11 @@
 #convert /tmp/lock.png -scale 10% -scale 1000% /tmp/lock.png
 #convert /tmp/lock.png  ~/.lock.png -gravity center -composite -matte /tmp/lock.png
 #i3lock -u -i /tmp/lock.png
-rm ~/.solid.png
-convert -size `xdpyinfo|grep dimensions|awk '{print $2}'` xc:\#2a1f1d ~/.solid.png
+#rm ~/.solid.png
+#convert -size `xdpyinfo|grep dimensions|awk '{print $2}'` xc:\#2a1f1d ~/.solid.png
 #convert ~/.solid.png ~/.lock.png -gravity center -composite -matte ~/.solid.png
-convert ~/.solid.png ~/.lock.png -geometry +50+800 -composite ~/.solid.png
-i3lock -u -i ~/.solid.png
+#convert ~/.solid.png ~/.lock.png -geometry +50+800 -composite ~/.solid.png
+#i3lock -u -i ~/.solid.png
+i3lock -u -c 2c2422
+
 
