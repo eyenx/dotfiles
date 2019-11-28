cb27da7aa26ce97d37655f8e1078854ac0130bf3
eye <eye@eyenx.ch>
2019-11-28 11:47:45 +0100

    fix bin/lock.sh

--- bin/lock.sh
+++ bin/lock.sh
@@ -2,10 +2,12 @@
 # a lot taken from https://github.com/xero/glitchlock
 
 # get screen measures
+rm /tmp/lock*.png
 scrot /tmp/lock.png
 W=`identify -ping -format '%w' /tmp/lock.png`
 H=`identify -ping -format '%h' /tmp/lock.png`
-convert -size $Wx$H canvas:\#362d24 /tmp/lock.png
+
+convert -size ${W}x${H} canvas:\#362d24 /tmp/lock.png
 
 ICON="/home/eye/img/stop.png"
 LOCK=()
@@ -30,5 +32,6 @@ file=/tmp/lock.png
 # put icon on it
 convert "$file" "${LOCK[@]}" "$file"
 
+
 # lock 
-i3lock -n --bar-indicator --bar-position h --bar-direction 1 --redraw-thread -t "" --bar-step 50 --bar-width 250 --bar-base-width 50 --bar-max-height 100 --bar-periodic-step 50 --bar-color 362d24ff --keyhlcolor 00666666 --ringvercolor cc87875f --wrongcolor ffff0000 --veriftext="" --wrongtext="" --noinputtext="" -i ~/img/lock.png
+i3lock -n --bar-indicator --bar-position h --bar-direction 1 --redraw-thread -t "" --bar-step 50 --bar-width 250 --bar-base-width 50 --bar-max-height 100 --bar-periodic-step 50 --bar-color 362d24ff --keyhlcolor 00666666 --ringvercolor cc87875f --wrongcolor ffff0000 --veriftext="" --wrongtext="" --noinputtext="" -i "$file"
