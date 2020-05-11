53b34659275126bb5301e045d83c31dc40d44c4c
eye <eye@eyenx.ch>
2020-05-11 18:37:28 +0200

    mocha color theme

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -87,8 +87,8 @@ altMask     = mod1Mask
 myWorkspaces =  ["1","2","3","4","5","6","7","8","9"]
 
 -- border colors
-myNormalBorderColor  = "#e6c6b0"
-myFocusedBorderColor = "#9f7155"
+myNormalBorderColor  = "#d0c8c6"
+myFocusedBorderColor = "#bb9584"
 
 --key bindings
 myKeys = \c -> mkKeymap c $ 
@@ -236,15 +236,15 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 
 -- TabConfig
 
-myTabConfig = def { activeColor = "#30200f"
-                  , inactiveColor = "#30200f"
-                  , urgentColor = "#9f155"
-                  , activeBorderColor = "#30200f"
-                  , inactiveBorderColor = "#30200f"
-                  , urgentBorderColor = "#9f155"
-                  , activeTextColor = "#9f7155"
-                  , inactiveTextColor = "#e6c6b0"
-                  , urgentTextColor = "#30200f"
+myTabConfig = def { activeColor = "#3b3228"
+                  , inactiveColor = "#3b3228"
+                  , urgentColor = "#bb9584"
+                  , activeBorderColor = "#3b3228"
+                  , inactiveBorderColor = "#3b3228"
+                  , urgentBorderColor = "#bb9584"
+                  , activeTextColor = "#bb9584"
+                  , inactiveTextColor = "#b8afad"
+                  , urgentTextColor = "#3b3228"
                   , fontName = "xft:Liberation Mono:size=8"
                   }
 --layouts
@@ -318,12 +318,12 @@ myXmobar = "xmobar /home/eye/.xmobarrc"
 
 -- statusbar / logging
 myLogHook h = dynamicLogWithPP $ def {
-        ppCurrent = xmobarColor "#9f7155" ""
-        , ppHidden = xmobarColor "#e6c6b0" "" 
-        , ppUrgent = xmobarColor "#9f155" "#573d26" 
-        , ppSep = xmobarColor "#9f7155" "" " · "
+        ppCurrent = xmobarColor "#bb9584" ""
+        , ppHidden = xmobarColor "#b8afad" "" 
+        , ppUrgent = xmobarColor "#bb9584" "#573d26" 
+        , ppSep = xmobarColor "#bb9584" "" " · "
         , ppWsSep = xmobarColor "#999999" "" " "
-        , ppTitle = xmobarColor "#e6c6b0" "" . shorten 50
+        , ppTitle = xmobarColor "#b8afad" "" . shorten 50
         -- do not show NSP at end of workspace list
         , ppSort = fmap (.namedScratchpadFilterOutWorkspace) $ ppSort def
         , ppOutput = hPutStrLn h
