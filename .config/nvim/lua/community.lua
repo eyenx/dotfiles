7687bb592aaf292dcffe1a9c23f116930e77cf50
Toni Tauro <toni.tauro@adfinis.com>
2026-01-19 07:43:00 +0100

    chore: update

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -4,13 +4,10 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.terraform" },
   { import = "astrocommunity.pack.helm" },
-  --  { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.editing-support.chatgpt-nvim" },
-  --  { import = "astrocommunity.note-taking.zk-nvim" },
-  -- { import = "astrocommunity.note-taking.neorg" },
   { import = "astrocommunity.note-taking.obsidian-nvim" },
 }
