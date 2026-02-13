a318f182801c0eca91f6b49d67be438ec9c52626
Toni Tauro <toni.tauro@adfinis.com>
2026-02-13 07:31:37 +0100

    chore: update
    Signed-off-by: Toni Tauro <toni.tauro@adfinis.com>

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -7,7 +7,9 @@ return {
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.nix" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.ai.opencode-nvim" },
   { import = "astrocommunity.editing-support.chatgpt-nvim" },
   { import = "astrocommunity.note-taking.obsidian-nvim" },
 }
--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -2,10 +2,19 @@ return { -- override nvim-cmp plugin
   "hrsh7th/nvim-cmp",
   -- override the options table that is used in the `require("cmp").setup()` call
   opts = function(_, opts)
-    -- opts parameter is the default options table
-    -- the function is lazy loaded so cmp is able to be required
+    --    opts.sources = {
+    --      {
+    --        { name = "minuet" },
+    --      },
+    --    }
+    --    opts.performance = {
+    --      fetching_timeout = 2000,
+    --    }
     local cmp = require "cmp"
     -- modify the mapping part of the table
-    opts.mapping["<C-y>"] = cmp.mapping.confirm { select = true }
+    opts.mapping = vim.tbl_deep_extend("force", opts.mapping or {}, {
+      ["<C-y>"] = cmp.mapping.confirm { select = true },
+      --      ["<A-y>"] = require("minuet").make_cmp_map(),
+    })
   end,
 }
